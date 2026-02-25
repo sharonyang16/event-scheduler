d6c9fec49cb2680c7a5335dfe6da9a2c4baa2ee6
Sharon Yang <sharonyyang0@gmail.com>
2026-02-25 10:49:53 -0500

    add keychain manager

---
+++ event-scheduler/Utils/KeychainManager.swift
@@ -0,0 +1,53 @@
+//
+//  KeychainManager.swift
+//  event-scheduler
+//
+//  Created by Sharon Yang on 2/25/26.
+//
+
+import Security
+import Foundation
+
+struct KeychainManager {
+    static let service = "com.eventscheduler"
+    
+    static func save(value: String, key: String) -> Void {
+        let data = Data(value.utf8)
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        
+        SecItemAdd(query as CFDictionary, nil)
+    }
+    
+    static func get(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        SecItemCopyMatching(query as CFDictionary, &result)
+            
+        guard let data = result as? Data else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+    
+    static func delete(key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+    }
+}
